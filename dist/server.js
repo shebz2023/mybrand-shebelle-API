5c1f0b4215d3655d7d1cf1290baaa53088af90ff
shebz2023 <shebelleke@gmail.com>
2025-02-09 13:14:20 +0200

    fx(mongourl):compiled for new changes after using env

--- dist/server.js
+++ dist/server.js
@@ -14,35 +14,36 @@ const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 7000;
-const MONGO_URL = `mongodb+srv://shebz:shebz123@cluster0.iwskxk4.mongodb.net/blogdb?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL || '';
 app.use(body_parser_1.default.json());
 // Enable CORS
 app.use((0, cors_1.default)({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 // swagger
-const swaggerJsDocs = yamljs_1.default.load('./api.yaml');
-app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerJsDocs));
+const swaggerJsDocs = yamljs_1.default.load("./api.yaml");
+app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerJsDocs));
 // Routes
-app.use('/', routes_1.default);
+app.use("/", routes_1.default);
 // Healthy check
-app.get('/ping', (req, res) => {
+app.get("/ping", (req, res) => {
     res.json({ "you are": "free to go" });
 });
 // no request
 app.use((req, res, next) => {
-    res.status(404).json({ error: 'check the request again' });
+    res.status(404).json({ error: "check the request again" });
 });
-mongoose_1.default.connect(MONGO_URL)
+mongoose_1.default
+    .connect(MONGO_URL)
     .then(() => {
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}🚀`);
     });
 })
     .catch((err) => {
-    console.error('MongoDB Connection Error: ', err);
+    console.error("MongoDB Connection Error: ", err);
 });
 exports.default = app;
