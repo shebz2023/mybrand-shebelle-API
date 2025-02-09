3310c39abda10adc4b3fa66709f76ecb79b9e70c
shebz2023 <shebelleke@gmail.com>
2025-02-09 12:53:55 +0200

    used environment variable for the connection string

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,55 @@
-import express, { Request, Response, NextFunction } from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser'; 
-import routes from './routes/routes';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs'; 
-import cors from 'cors'; 
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import routes from "./routes/routes";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 7000;
-const MONGO_URL = `mongodb+srv://shebz:shebz123@cluster0.iwskxk4.mongodb.net/blogdb?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL || '';
 
 app.use(bodyParser.json());
 
 // Enable CORS
-app.use(cors({
-    origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // swagger
-const swaggerJsDocs = YAML.load('./api.yaml'); 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
+const swaggerJsDocs = YAML.load("./api.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
 
 // Routes
-app.use('/', routes);
+app.use("/", routes);
 
 // Healthy check
-app.get('/ping', (req, res) => {
-    res.json({ "you are": "free to go" });
+app.get("/ping", (req, res) => {
+  res.json({ "you are": "free to go" });
 });
 // no request
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({ error: 'check the request again' });
+  res.status(404).json({ error: "check the request again" });
 });
 
-mongoose.connect(MONGO_URL)
-    .then(() => {
-        console.log('MongoDB Connected');
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error('MongoDB Connection Error: ', err);
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}🚀`);
     });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error: ", err);
+  });
 
 export default app;
